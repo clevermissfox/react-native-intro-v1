692dc342eaf71bae88ca3f77707b4c2e0684938b
CeeCeee <edicodesigner@gmail.com>
2025-04-16 21:18:03 -0700

    initial setup

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { COLORS, PADDING, SIZE } from "./src/theme/tokens";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> helloooooo</Text>
+      <Text> hello friend</Text>
+      <View style={[styles.header, styles.wrapper]}>
+        <Text>This is the Header</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: COLORS.neutral100,
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    flex: 1,
+    alignSelf: "stretch",
+    backgroundColor: COLORS.neutral900,
+    paddingBlock: PADDING.large,
+    paddingInline: PADDING.medium,
+  },
+  wrapper: {
+    width: "min(100%, 60ch)",
+    marginInline: "auto",
   },
 });
---
+++ src/theme/tokens.js
@@ -0,0 +1,18 @@
+// Using in place of css custom properties
+export const COLORS = {
+  neutral100: "fff",
+  neutral900: "#272727",
+};
+
+export const PADDING = {
+  xSmall: "0.25em",
+  small: "0.5em",
+  medium: "1em",
+  default: "1.5em",
+  large: "2em",
+  xLarge: "2.5em",
+};
+
+export const SIZE = {
+  wrapperWidth: "min(100%, 60ch)",
+};
